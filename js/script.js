d5c9070040c0a7576caaea1241e2db444d1cd80e
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-18 13:48:29 -0300

    add e remove tarefa

--- js/script.js
+++ js/script.js
@@ -10,9 +10,16 @@ document.querySelector('#entrada').onclick = function() {
                     ${document.querySelector('#nova-tarefa input').value}
                 </span>
                 <button class="deletar">
-                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAqklEQVRIS2NkoDFgpLH5DIQscAA6YD4QK+BwyAOgeCIQH8DlUEIWgAyQJ+BLkBpFci34D9WIyyGE5AkGESEDCMljWADTQGncw32M7nWaWwBzOUGvo3kRp3qyI4/aFqC7kBAfbj+xPiBkIMVBNGoBRr4iNUhG44Bg0URxEBGygWQLPgBN5CdkKpr8Q2w1H66cDKoqFxBRm8HsABmegK3qJFRlkugJTOU0twAAUHk4GewN7TMAAAAASUVORK5CYII="/>
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAuElEQVRIS2NkoDFgpLH5DHgt+P//vwPQAfOBWAGHQx4AxRMZGRkP4HIoIQtABsgT8OUDoAWK5FrwH6QRaABWhwB9iFcerBef6wgZQEgewwKYBkojHtnHKD6guQUwlxPjdWRf4lNPduRR1QJ0FxLiI1tOlA8IGUhxEI1agJGvSA2S0TggWDRRHESEbCDHgg9AQ/kJGYwm/xBYimLUfLhyMqiqXEBEbQaz4yGQkYCt6hzYSp/EIMKqHADevM4Z3dg9agAAAABJRU5ErkJggg=="/>
                 </button>
             </div>
         `;
+
+        var tarefaAtual = document.querySelectorAll(".deletar");
+        for(var i=0; i<tarefaAtual.length; i++){
+            tarefaAtual[i].onclick = function() {
+                this.parentNode.remove();
+            };
+        }
     }
 }
