5ea4655089ec98b870830e9259832345aa09f231
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-18 13:38:28 -0300

    .

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.querySelector('#entrada').onclick = function() {
                     ${document.querySelector('#nova-tarefa input').value}
                 </span>
                 <button class="deletar">
-                    
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAqklEQVRIS2NkoDFgpLH5DIQscAA6YD4QK+BwyAOgeCIQH8DlUEIWgAyQJ+BLkBpFci34D9WIyyGE5AkGESEDCMljWADTQGncw32M7nWaWwBzOUGvo3kRp3qyI4/aFqC7kBAfbj+xPiBkIMVBNGoBRr4iNUhG44Bg0URxEBGygWQLPgBN5CdkKpr8Q2w1H66cDKoqFxBRm8HsABmegK3qJFRlkugJTOU0twAAUHk4GewN7TMAAAAASUVORK5CYII="/>
                 </button>
             </div>
         `;
