2d59dae7e75886f8368b93143fe037100a0e8b8d
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-18 13:22:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.querySelector('#entrada').onclick = function() {
-    
+    if (document.querySelector('#nova-tarefa input').value.length == 0){
+        alert('Por favor insira uma tarefa!');
+    }
+    else {
+        document.querySelector('#lista-tarefas').innerHTML 
+        += `
+            <div class="tarefa">
+                <span id="tarefa-nome">
+                    ${document.querySelector('#nova-tarefa input').value}
+                </span>
+            </div>
+        `;
+    }
 }
